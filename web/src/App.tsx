07ff7026082ce823a3e495d268be57fc0788235e
Luca Scalzotto <luca@scorpiac.com>
2021-05-13 11:25:03 +0200

    Sass, router, remove unused stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/auth/login" component={LoginPage} />
+      <Redirect to="/auth/login" />
+    </Switch>
   );
 }
-
-export default App;
---
+++ web/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoginPage: React.FC = () => {
+  return (
+    <div className="login-page">
+      Log in to ClearCloud
+    </div>
+  )
+}
