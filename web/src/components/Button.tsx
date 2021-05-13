6c9db6b25145ca40915c58a7a15e73c7bada8972
Luca Scalzotto <luca@scorpiac.com>
2021-05-13 15:51:46 +0200

    Add form, error message

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { classNames } from '../utils/classNames';
 
 export interface Props {
-  onClick: () => void;
+  onClick?: () => void;
   square?: boolean;
+  type?: 'button' | 'submit';
 }
 
-export const Button: React.FC<Props> = ({ children, onClick, square }) => (
+export const Button: React.FC<Props> = ({
+  children,
+  onClick,
+  square,
+  type = 'button',
+}) => (
   <button
     className={classNames('button', square && 'square')}
     onClick={onClick}
+    type={type}
   >
     {children}
   </button>
--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -3,42 +3,49 @@ import { TextInput } from '../components/input/TextInput';
 import { Button } from '../components/Button';
 import { Icon } from '../components/Icon';
 import { AuthService } from '../services/AuthService';
+import { Form } from '../components/input/Form';
 
 export const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
   const authService = new AuthService();
 
   return (
     <div className="login-page">
       <div className="login-box">
         <div className="title">Welcome to ClearCloud</div>
-        <div className="input-row">
-          <TextInput
-            value={username}
-            onChange={setUsername}
-            placeholder="Username"
-          />
-        </div>
-        <div className="input-row password">
-          <TextInput
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={setPassword}
-            placeholder="Password"
-          />
-          <Button square onClick={() => setShowPassword(!showPassword)}>
-            <Icon icon={showPassword ? 'eye-slash' : 'eye'} />
-          </Button>
-        </div>
-        <Button
-          onClick={() => {
-            authService.login(username, password).then(console.log);
+        <Form
+          onSubmit={async () => {
+            setError(false);
+            return authService
+              .login(username, password)
+              .then(console.log)
+              .catch(() => setError(true));
           }}
+          submitLabel="Log in"
         >
-          Log in
-        </Button>
+          <div className="input-row">
+            <TextInput
+              value={username}
+              onChange={setUsername}
+              placeholder="Username"
+            />
+          </div>
+          <div className="input-row password">
+            <TextInput
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={setPassword}
+              placeholder="Password"
+            />
+            <Button square onClick={() => setShowPassword(!showPassword)}>
+              <Icon icon={showPassword ? 'eye-slash' : 'eye'} />
+            </Button>
+          </div>
+        </Form>
+        {error && <p className="error">Invalid username or password.</p>}
       </div>
     </div>
   );
