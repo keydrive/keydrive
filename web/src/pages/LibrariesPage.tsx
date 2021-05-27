aec8f7ffcefc8ff86e8eadd4577fec28fdc85179
Luca Scalzotto <luca@scorpiac.com>
2021-05-27 18:07:57 +0200

    Remove file details component

--- web/src/pages/LibrariesPage.tsx
+++ web/src/pages/LibrariesPage.tsx
@@ -4,40 +4,8 @@ import { useService } from '../hooks/useService';
 import { userStore } from '../store/user';
 import { Icon } from '../components/Icon';
 import { Link } from 'react-router-dom';
-import { FileDetails, FileInfo } from '../components/FileDetails';
 
-const libraries = ['Files', 'Photos', 'Movies', 'Music', 'Downloads', 'Personal'];
-
-const files: FileInfo[] = [
-  {
-    name: 'Downloads',
-    kind: 'folder',
-    lastModified: 'Yesterday',
-  },
-  {
-    name: 'Documents',
-    kind: 'folder',
-    lastModified: 'Yesterday',
-  },
-  {
-    name: 'Homework.docx',
-    kind: 'file',
-    lastModified: '23:32',
-    size: '5.2 MB',
-  },
-  {
-    name: 'Avatar.png',
-    kind: 'file',
-    lastModified: '7 Days ago',
-    size: '66 KB',
-  },
-  {
-    name: 'Linkin_Park_Numb.exe',
-    kind: 'file',
-    lastModified: '11:15',
-    size: '34 MB',
-  },
-];
+const libraries = ['Libraries go here!', 'Files', 'Photos', 'Movies', 'Music', 'Downloads', 'Personal'];
 
 export const LibrariesPage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -79,7 +47,7 @@ export const LibrariesPage: React.FC = () => {
             </div>
           )}
         </div>
-        <FileDetails files={files} />
+        Files go here!
       </div>
     </div>
   );
