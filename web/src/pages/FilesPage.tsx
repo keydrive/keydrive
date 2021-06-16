99fdaf0026be60b942e165191a9f4e3876ebc468
Thomas Biesaart <thomas.biesaart@protonmail.com>
2021-06-16 15:51:58 +0200

    Copy colors and names from designs

--- web/src/pages/FilesPage.tsx
+++ web/src/pages/FilesPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Layout } from '../components/Layout';
+import { Panel } from '../components/Panel';
 
 export const FilesPage: React.FC = () => {
   return (
-    <Layout className="files-page">
-      <div className="top-bar" />
-      <main>Files go here!</main>
+    <Layout className='files-page'>
+      <div className='top-bar' />
+      <main>
+        <Panel>
+          Files go here!
+        </Panel>
+      </main>
     </Layout>
   );
 };
