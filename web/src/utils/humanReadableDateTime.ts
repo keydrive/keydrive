4dc29195ca720f3c35d8cdb891a7bb307758b4a0
Luca Scalzotto <luca@scorpiac.com>
2021-07-02 00:03:09 +0200

    Add tests, use preset formatters with en-GB format

--- web/src/utils/humanReadableDateTime.ts
+++ web/src/utils/humanReadableDateTime.ts
@@ -1,17 +1,28 @@
+const dateFormatter = new Intl.DateTimeFormat('en-GB', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+});
+
+const timeFormatter = new Intl.DateTimeFormat('en-GB', {
+  hour: '2-digit',
+  minute: '2-digit',
+});
+
 export function humanReadableDateTime(dateString: string): string {
   const date = new Date(dateString);
   const now = new Date();
   const yesterday = new Date(now);
   yesterday.setDate(now.getDate() - 1);
 
-  let datePart = date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+  let datePart = dateFormatter.format(date);
   if (isSameDay(date, now)) {
     datePart = 'Today';
   } else if (isSameDay(date, yesterday)) {
     datePart = 'Yesterday';
   }
 
-  return `${datePart} at ${date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' })}`;
+  return `${datePart} at ${timeFormatter.format(date)}`;
 }
 
 function isSameDay(a: Date, b: Date): boolean {
