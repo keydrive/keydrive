aa4c6387bc7bf64135c129dbbacb8a3af247c56c
Thomas Biesaart <thomas.biesaart@protonmail.com>
2021-07-02 21:17:14 +0200

    Add more tests

--- internal/controller/Entries_test.go
+++ internal/controller/Entries_test.go
@@ -16,7 +16,7 @@ import (
 
 func TestDownloadEntry(t *testing.T) {
 	// Prep filesystem\
-	tempDir, _ := os.MkdirTemp("", "unit-test")
+	tempDir := t.TempDir()
 	_ = os.WriteFile(filepath.Join(tempDir, "gimme.txt"), []byte("Hello World!\n"), 0777)
 
 	// Mock database
@@ -34,14 +34,18 @@ func TestDownloadEntry(t *testing.T) {
 	fs := &service.FileSystem{}
 
 	controller := DownloadEntry(db, libs, fs)
-	// Go!
-	t.Run("it download a file", func(t *testing.T) {
+
+	setup := func() {
 		mock.
 			ExpectQuery("SELECT .* FROM \"libraries\" left join can_access_libraries on can_access_libraries.library_id = libraries.id WHERE .* ORDER BY id LIMIT 1").
 			WithArgs(675, 387).
 			WillReturnRows(sqlmock.NewRows([]string{"id", "type", "name", "root_folder", "can_write"}).AddRow(12, model.TypeGeneric, "Test Library", tempDir, true)).
 			RowsWillBeClosed()
+	}
 
+	// Go!
+	t.Run("it download a file", func(t *testing.T) {
+		setup()
 		recorder := httptest.NewRecorder()
 		c, _ := gin.CreateTestContext(recorder)
 		c.Params = []gin.Param{
@@ -75,11 +79,7 @@ func TestDownloadEntry(t *testing.T) {
 	})
 
 	t.Run("it returns 404 if file not found", func(t *testing.T) {
-		mock.
-			ExpectQuery("SELECT .* FROM \"libraries\" left join can_access_libraries on can_access_libraries.library_id = libraries.id WHERE .* ORDER BY id LIMIT 1").
-			WithArgs(675, 387).
-			WillReturnRows(sqlmock.NewRows([]string{"id", "type", "name", "root_folder", "can_write"}).AddRow(12, model.TypeGeneric, "Test Library", tempDir, true)).
-			RowsWillBeClosed()
+		setup()
 
 		recorder := httptest.NewRecorder()
 		c, _ := gin.CreateTestContext(recorder)
@@ -105,11 +105,7 @@ func TestDownloadEntry(t *testing.T) {
 	})
 
 	t.Run("it returns 404 if no access to library", func(t *testing.T) {
-		mock.
-			ExpectQuery("SELECT .* FROM \"libraries\" left join can_access_libraries on can_access_libraries.library_id = libraries.id WHERE .* ORDER BY id LIMIT 1").
-			WithArgs(675, 387).
-			WillReturnRows(sqlmock.NewRows([]string{"id", "type", "name", "root_folder", "can_write"}).AddRow(12, model.TypeGeneric, "Test Library", tempDir, true)).
-			RowsWillBeClosed()
+		setup()
 
 		recorder := httptest.NewRecorder()
 		c, _ := gin.CreateTestContext(recorder)
@@ -118,6 +114,50 @@ func TestDownloadEntry(t *testing.T) {
 				Key:   "libraryId",
 				Value: "23042",
 			},
+			{
+				Key:   "path",
+				Value: "gimme.txt",
+			},
+		}
+		c.Set(oauth.ContextKeyUser, model.User{
+			ID: 675,
+		})
+
+		controller(c)
+
+		if recorder.Code != 404 {
+			t.Errorf("Expected a 404 error but got %d instead", recorder.Code)
+		}
+	})
+
+	t.Run("it returns 404 if no path provided", func(t *testing.T) {
+		setup()
+
+		recorder := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(recorder)
+		c.Params = []gin.Param{
+			{
+				Key:   "libraryId",
+				Value: "23042",
+			},
+		}
+		c.Set(oauth.ContextKeyUser, model.User{
+			ID: 675,
+		})
+
+		controller(c)
+
+		if recorder.Code != 404 {
+			t.Errorf("Expected a 404 error but got %d instead", recorder.Code)
+		}
+	})
+
+	t.Run("it returns 404 if no libraryId provided", func(t *testing.T) {
+		setup()
+
+		recorder := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(recorder)
+		c.Params = []gin.Param{
 			{
 				Key:   "path",
 				Value: "gimme.txt",
