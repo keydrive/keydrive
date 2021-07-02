f347c5ee017b3bc1ca2ed57a8a8643698353638f
Thomas Biesaart <thomas.biesaart@protonmail.com>
2021-07-02 22:26:53 +0200

    Add styling to login page

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Layout } from '../components/Layout';
+import { useAppSelector } from '../store';
+import { useService } from '../hooks/useService';
+import { librariesStore } from '../store/libraries';
+import { Redirect } from 'react-router-dom';
 
-export const HomePage: React.FC = () => (
-  <Layout>
-    <div className="top-bar" />
-  </Layout>
-);
+export const HomePage: React.FC = () => {
+  const {
+    selectors: { libraries },
+  } = useService(librariesStore);
+  const libs = useAppSelector(libraries);
+  if (libs && libs[0]) {
+    return <Redirect to={`/files/${libs[0].id}`} />;
+  } else {
+    return <Redirect to="/settings" />;
+  }
+};
