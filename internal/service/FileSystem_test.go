9f3c1a25149c1e8da90116106c4dba54fca9fb64
Thomas Biesaart <thomas.biesaart@protonmail.com>
2021-07-02 21:38:29 +0200

    Add another test

--- internal/service/FileSystem_test.go
+++ internal/service/FileSystem_test.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"clearcloud/internal/model"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestFileSystem_OpenFile(t *testing.T) {
+	tmpDir := t.TempDir()
+	_ = os.WriteFile(filepath.Join(tmpDir, "test.bin"), []byte{1, 2, 3}, 0777)
+	lib := model.Library{
+		RootFolder: tmpDir,
+	}
+	fs := FileSystem{}
+	stream, err := fs.OpenFile(lib, "test.bin")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	data, _ := io.ReadAll(stream)
+	if data[0] != 1 || data[1] != 2 || data[2] != 3 {
+		t.Errorf("Invalid bytes came back")
+	}
+}
