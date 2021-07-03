06c31b4be2b935f3abc812c361e2d8b2a1c890e0
Thomas Biesaart <thomas.biesaart@protonmail.com>
2021-07-03 11:38:22 +0200

    Add postgres sidecar container for tests

--- internal/controller/Entries_test.go
+++ internal/controller/Entries_test.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	"clearcloud/internal/model"
-	"net/http"
+	"fmt"
 	"net/http/httptest"
 	"os"
 	"path/filepath"
@@ -23,7 +23,7 @@ func TestDownloadEntry(t *testing.T) {
 
 	// Go!
 	t.Run("it downloads a file", func(t *testing.T) {
-		req, _ := http.NewRequest("GET", "/api/libraries/5764/entries/gimme.txt/download", nil)
+		req := adminRequest("GET", fmt.Sprintf("/api/libraries/%d/entries/gimme.txt/download", lib.ID), nil)
 		recorder := httptest.NewRecorder()
 		testApp.Router.ServeHTTP(
 			recorder,
@@ -45,15 +45,42 @@ func TestDownloadEntry(t *testing.T) {
 	})
 
 	t.Run("it returns 404 if file not found", func(t *testing.T) {
-
+		req := adminRequest("GET", fmt.Sprintf("/api/libraries/%d/entries/nope.txt/download", lib.ID), nil)
+		recorder := httptest.NewRecorder()
+		testApp.Router.ServeHTTP(
+			recorder,
+			req,
+		)
+		status := recorder.Code
+		if status != 404 {
+			t.Errorf("Expected 404 but got: %d", status)
+		}
 	})
 
 	t.Run("it returns 404 if no access to library", func(t *testing.T) {
-
+		req := noAccessUserRequest("GET", fmt.Sprintf("/api/libraries/%d/entries/gimme.txt/download", lib.ID), nil)
+		recorder := httptest.NewRecorder()
+		testApp.Router.ServeHTTP(
+			recorder,
+			req,
+		)
+		status := recorder.Code
+		if status != 404 {
+			t.Errorf("Expected 404 but got: %d", status)
+		}
 	})
 
 	t.Run("it returns 400 if no path provided", func(t *testing.T) {
-
+		req := adminRequest("GET", fmt.Sprintf("/api/libraries/%d/entries//download", lib.ID), nil)
+		recorder := httptest.NewRecorder()
+		testApp.Router.ServeHTTP(
+			recorder,
+			req,
+		)
+		status := recorder.Code
+		if status != 400 {
+			t.Errorf("Expected 400 but got: %d", status)
+		}
 	})
 
 	t.Run("it returns 404 if no libraryId provided", func(t *testing.T) {
@@ -61,6 +88,16 @@ func TestDownloadEntry(t *testing.T) {
 	})
 
 	t.Run("it returns 400 if path not encoded", func(t *testing.T) {
-
+		req := adminRequest("GET", "/simple", nil)
+		req.URL.Path = fmt.Sprintf("/api/libraries/%d/entries/what is this sh%%it!/download", lib.ID)
+		recorder := httptest.NewRecorder()
+		testApp.Router.ServeHTTP(
+			recorder,
+			req,
+		)
+		status := recorder.Code
+		if status != 400 {
+			t.Errorf("Expected 400 but got: %d", status)
+		}
 	})
 }
