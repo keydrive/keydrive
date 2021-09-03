5e8810d91edf933662868b0bd060320aec8d6264
Luca Scalzotto <luca@scorpiac.com>
2021-09-03 20:17:53 +0200

    Move details panel actions to context menu

--- web/src/components/files/EntryDetailsPanel.tsx
+++ web/src/components/files/EntryDetailsPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Entry } from '../../services/LibrariesService';
 import { Panel } from '../Panel';
 import { EntryIcon } from '../EntryIcon';
@@ -6,6 +6,8 @@ import { ButtonGroup } from '../ButtonGroup';
 import { Button } from '../Button';
 import { humanReadableDateTime } from '../../utils/humanReadableDateTime';
 import { humanReadableSize } from '../../utils/humanReadableSize';
+import { IconButton } from '../IconButton';
+import { ContextMenu } from '../ContextMenu';
 
 export interface Props {
   entry: Entry;
@@ -14,41 +16,52 @@ export interface Props {
   onDelete: () => void;
 }
 
-export const EntryDetailsPanel: React.FC<Props> = ({ entry, onDownload, onRename, onDelete }) => (
-  <>
-    <Panel className="info">
-      <div className="preview">
-        <EntryIcon entry={entry} />
-      </div>
-      <div className="name">{entry.name}</div>
-      <div className="category">{entry.category}</div>
-    </Panel>
-    <Panel className="actions">
-      <ButtonGroup fullWidth vertical>
-        {entry.category !== 'Folder' && (
-          <Button onClick={onDownload} icon="download">
-            Download
-          </Button>
-        )}
-        <Button onClick={onRename} icon="pencil-alt">
-          Rename
-        </Button>
-        <Button onClick={onDelete} icon="trash">
-          Delete
-        </Button>
-      </ButtonGroup>
-    </Panel>
-    <Panel className="metadata">
-      <div>
-        <span>Modified</span>
-        <span>{humanReadableDateTime(entry.modified)}</span>
-      </div>
-      {entry.category !== 'Folder' && (
-        <div>
-          <span>Size</span>
-          <span>{humanReadableSize(entry.size)}</span>
+export const EntryDetailsPanel: React.FC<Props> = ({ entry, onDownload, onRename, onDelete }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <Panel className="info">
+        <div className="preview">
+          <EntryIcon entry={entry} />
         </div>
-      )}
-    </Panel>
-  </>
-);
+        <div className="name-actions">
+          <div>
+            <div className="name">{entry.name}</div>
+            <div className="category">{entry.category}</div>
+          </div>
+          <IconButton icon="ellipsis-h" onClick={() => setShowMenu((prevState) => !prevState)} />
+        </div>
+        {showMenu && (
+          <ContextMenu onClose={() => setShowMenu(false)}>
+            <ButtonGroup vertical>
+              {entry.category !== 'Folder' && (
+                <Button onClick={onDownload} icon="download">
+                  Download
+                </Button>
+              )}
+              <Button onClick={onRename} icon="pencil-alt">
+                Rename
+              </Button>
+              <Button onClick={onDelete} icon="trash">
+                Delete
+              </Button>
+            </ButtonGroup>
+          </ContextMenu>
+        )}
+      </Panel>
+      <Panel className="metadata">
+        <div>
+          <span>Modified</span>
+          <span>{humanReadableDateTime(entry.modified)}</span>
+        </div>
+        {entry.category !== 'Folder' && (
+          <div>
+            <span>Size</span>
+            <span>{humanReadableSize(entry.size)}</span>
+          </div>
+        )}
+      </Panel>
+    </>
+  );
+};
