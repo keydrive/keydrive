03bc8c22d4fa2d405414bb4fadd685a581d2dd51
Luca Scalzotto <luca@scorpiac.com>
2022-03-26 11:34:59 +0100

    Remove unused web-vitals package

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
 import { App } from './App';
-import { reportWebVitals } from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { initializeStore } from './store';
@@ -29,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
