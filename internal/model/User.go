d4e63ecb96b7a8db0f14248881de3be5e721a52d
Luca Scalzotto <luca@scorpiac.com>
2023-06-09 20:55:15 +0200

    Update all go-related things, exclude user isAdmin column from automigration

--- internal/model/User.go
+++ internal/model/User.go
@@ -5,7 +5,9 @@ type User struct {
 	Username       string `json:"username" gorm:"not null;unique;type:citext"`
 	Name           string `json:"name" gorm:"not null;default:''"`
 	HashedPassword string `json:"-" gorm:"not null"`
-	IsAdmin        bool   `json:"isAdmin" gorm:"->;type:boolean GENERATED ALWAYS AS (id = 1) STORED"`
+
+	// Note that this column is ignored from automigration, because generated columns cannot be altered.
+	IsAdmin bool `json:"isAdmin" gorm:"->;type:boolean GENERATED ALWAYS AS (id = 1) STORED;-:migration"`
 }
 
 func (u User) GetUsername() string {
