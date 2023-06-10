f68275979c0573fd95f873febde2ba2d8bfde504
Luca Scalzotto <luca@scorpiac.com>
2023-06-10 23:39:23 +0200

    Revert "Make index redirect stricter (#154)"
    This reverts commit 283ba6c5b42421d4677c7356d710318471712297.

--- internal/controller/Static.go
+++ internal/controller/Static.go
@@ -4,26 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func Static(fs http.FileSystem) gin.HandlerFunc {
 	fileServer := http.FileServer(fs)
 
 	return func(c *gin.Context) {
-		path := c.Request.URL.Path
+		// Check if the file exists, serving the index if it does not.
+		if _, err := fs.Open(c.Request.URL.Path); os.IsNotExist(err) {
+			defer func(old string) {
+				c.Request.URL.Path = old
+			}(c.Request.URL.Path)
 
-		if c.Request.Method == http.MethodGet && !strings.HasPrefix(path, "/api/") && !strings.HasPrefix(path, "/oauth2/") {
-			// Check if the file exists, serving the index if it does not.
-			if _, err := fs.Open(path); os.IsNotExist(err) {
-				defer func(old string) {
-					c.Request.URL.Path = old
-				}(path)
-
-				// Overwrite the path with single slash, to let the file server serve index.html.
-				// Note that the path can't be `index.html`, since the file server will redirect to `./` in that case.
-				c.Request.URL.Path = "/"
-			}
+			// Overwrite the path with single slash, to let the file server serve index.html.
+			// Note that the path can't be `index.html`, since the file server will redirect to `./` in that case.
+			c.Request.URL.Path = "/"
 		}
 
 		fileServer.ServeHTTP(c.Writer, c.Request)
