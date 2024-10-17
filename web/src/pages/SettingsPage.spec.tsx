5c47dfd854688053f7d99caa373f4b42b3f0f5c2
Luca Scalzotto <luca@scalzotto.nl>
2024-10-17 16:40:45 +0200

    Add libraries mock to SettingsPage test

--- web/src/pages/SettingsPage.spec.tsx
+++ web/src/pages/SettingsPage.spec.tsx
@@ -2,9 +2,18 @@ import { ReallyDeepPartial, render } from '../__testutils__/render';
 import { RootState } from '../store';
 import { SettingsPage } from './SettingsPage';
 import { fireEvent, screen } from '@testing-library/react';
+import { checkPendingMocks } from '../__testutils__/checkPendingMocks.ts';
+import fetchMock from 'fetch-mock';
 
-describe.skip('SettingsPage', () => {
+describe('SettingsPage', () => {
+  afterEach(checkPendingMocks);
+
   it('redirects to other routes when modals are clicked', async () => {
+    fetchMock.getOnce('end:/api/libraries/?limit=100', {
+      totalElements: 0,
+      elements: [],
+    });
+
     const initialState: ReallyDeepPartial<RootState> = {
       libraries: {
         libraries: [],
@@ -18,12 +27,14 @@ describe.skip('SettingsPage', () => {
         },
       },
     };
+
     const { navigation } = await render(<SettingsPage />, {
       path: '/settings/nope',
       route: '/settings/:setting',
       loggedIn: true,
       initialState,
     });
+
     expect(navigation.pathname).toBe('/settings/nope');
     fireEvent.click(screen.getByText('Change Password'));
     expect(navigation.pathname).toBe('/settings/password');
